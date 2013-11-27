fbe97612cc4887d05c085690b1859071d95e970b
Aaron Stewart <aaron@binarypaean.com>
2013-11-27 11:14:23 -0800

    basic functionality

--- tables.js
+++ tables.js
@@ -1,7 +1,11 @@
 $( document ).ready( function () {
-
 	console.log("tables.js is loaded correctly.")
-	var projects = $( ".dfwp-column.dfwp-list" ).children();
-	console.log ("Found " + projects.length + " projects.");
+
+	var projectHeaderList = $( "ul.dfwp-column.dfwp-list" ).first();
+	var projectList = $( "ul.dfwp-column.dfwp-list" ).last();
+	//"dfwp-column dfwp-list" UL's are used twice on the page - first for the "active" header, second for everything else.
+	var projects = projectList.children();
+	var projectListNotification = $("<h4>" + "You have " + projects.length + " active projects." + "</h4>");
+	$(projectHeaderList).before( projectListNotification )
 
 });
