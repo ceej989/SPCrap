2ab6e825423112a2c369d976dd2c67b2da85243d
Aaron Stewart <aaron@binarypaean.com>
2013-11-27 10:58:50 -0800

    Got selector for projects working

--- tables.js
+++ tables.js
@@ -1,3 +1,7 @@
 $( document ).ready( function () {
-console.log("tables.js is loaded correctly.")
+
+	console.log("tables.js is loaded correctly.")
+	var projects = $( ".dfwp-column.dfwp-list" ).children();
+	console.log ("Found " + projects.length + " projects.");
+
 });
